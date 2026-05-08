7a8f7ab05a366f4672146b43538870c158b1c406
Apple <apple@Apples-MacBook-Air.local>
2026-05-08 07:58:30 +0530

    Add JS basics exercises and 08_may practice files; remove empty file

---
+++ 08_may/01.js
@@ -0,0 +1,13 @@
+var $=10;
+var _a=12;
+
+var p=10;
+
+var abc123=21
+
+console.log($);
+console.log(_a);
+console.log(p);
+console.log(abc123);        
+
+
---
+++ 08_may/shprtcut.js
@@ -0,0 +1,16 @@
+// Action	Shortcut
+// Copy	Cmd+C
+// Paste	Cmd+V
+// Cut	Cmd+X
+// Undo	Cmd+Z
+// Redo	Cmd+Shift+Z
+// Save	Cmd+S
+// Find	Cmd+F
+// Switch apps	Cmd+Tab
+// Force quit	Cmd+Opt+Esc
+// Screenshot	Cmd+Shift+4
+// Screenshot (window)	Cmd+Shift+4 then Space
+// Hide window	Cmd+H
+// Quit app	Cmd+Q
+// Spotlight	Cmd+Space
+// Lock screen	Cmd+Ctrl+Q
